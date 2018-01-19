c54208dd257144398413780a64b84c54a736d00d
A664164 <A664164@MC0YG9JC.ww930.my-it-solutions.net>
2018-01-19 17:29:08 +0530

    Spring MVC

--- src/com/wan/Dao/EmpDao.java
+++ src/com/wan/Dao/EmpDao.java
@@ -1,5 +1,23 @@
 package com.wan.Dao;
 
-public class EmpDao {
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.wan.beans.Emp;
+
+public class EmpDao
+{
+	JdbcTemplate template;
+	public void setTemplate(JdbcTemplate template) 
+	{  
+		this.template = template;   
+	}
+	
+	public int save(Emp e)
+	{
+		System.out.println(e.getId()+e.getName()+e.getSalary()+e.getDesignation());
+		
+		String sql= "insert into emp(id,name,salary,designation) values('"+e.getId()+"','"+e.getName()+"','"+e.getSalary()+"','"+e.getDesignation()+"')";
+		return template.update(sql);
+	}
+	
 }
